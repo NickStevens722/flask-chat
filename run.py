d1d1fb967661ab62b49a2d0a2744ccc981ed1c9b
Nick Stevens <nicholasstevens@hotmail.co.uk>
2020-04-08 09:30:09 +0000

    Added basic chat functionality

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
+
+
+def add_messages(username, message):
+    messages.append("{}: {}".format(username, message))
+
+
+def get_all_messages():
+    return "<br>".join(messages)
 
 
 @app.route('/')
@@ -12,12 +21,13 @@ def index():
 
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
